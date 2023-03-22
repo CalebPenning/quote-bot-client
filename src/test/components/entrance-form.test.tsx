d852aeae2833c5f937f04070db74e6fde705f8e7
Caleb Penning <calebpenning@mail.com>
2023-03-22 15:40:44 -0700

    remove empty test file for now

--- src/test/components/entrance-form.test.tsx
+++
@@ -1,8 +0,0 @@
-import EntranceForm from "../../components/entrance-form"
-// import { render, screen } from "../utils/test-utils"
-
-// describe("Entrance form", () => {
-// 	it("should have an input", () => {
-// 		render(<EntranceForm />)
-// 	})
-// })
